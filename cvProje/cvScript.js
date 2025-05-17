89aaf4a39d05114ce04368731b6cc944cb17ea92
yunuscanp <127553475+yunuscanp@users.noreply.github.com>
2025-05-17 15:22:18 +0300

    Update cvScript.js

--- cvProje/cvScript.js
+++ cvProje/cvScript.js
@@ -20,13 +20,6 @@ function generateCV() {
         setTimeout(() => {
             html2pdf()
                 .from(cvDiv)
-                .set({
-                    margin: 0.5,
-                    filename: "cv.pdf",
-                    image: { type: "jpeg", quality: 0.98 },
-                    html2canvas: { scale: 2, useCORS: true },
-                    jsPDF: { unit: "in", format: "a4", orientation: "portrait" }
-                })
                 .save()
                 .then(() => {
                     setTimeout(() => {
